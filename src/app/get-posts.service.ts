f46a8ca61fea67042fa83a007747a21793df8df5
kunal-visoulia <kunalvisoulia@gmail.com>
2019-01-26 02:03:33 +0530

    implemented get mock-heroes as service

--- src/app/get-posts.service.ts
+++ src/app/get-posts.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';  // Import it up here
+import { Hero } from './heroes/hero';
+import { HEROES } from './heroes/mock-heroes';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -17,5 +20,7 @@ export class GetPostsService {
   getUser(userId) {
     return this.http.get('https://jsonplaceholder.typicode.com/users/'+userId)
   }
-
+getHeroes():Observable<Hero[]>{
+  return of(HEROES);
+}
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from './hero'
-import {HEROES} from './mock-heroes'
+import { GetPostsService } from '../get-posts.service';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -11,14 +11,19 @@ hero:Hero={
   id:1,
   name:'Windstorm'
 };
-heroes=HEROES;
+heroes:Hero[];
 selectedHero: Hero;
 
-  constructor() { }
+  constructor(private gps: GetPostsService ) { }
+// defines a private gps property and identifies it as a HeroService injection site.
 
   ngOnInit() {
+    this.getHeroes();
   }
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
+
+  getHeroes(): void {
+this.gps.getHeroes().subscribe(heroes=>this.heroes=heroes);  }
 }
